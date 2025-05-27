6217aa7462c4245008215ce785f150e20f8f175c
Delaksan-Sritharan <delaksan@gmail.com>
2025-05-27 22:13:52 +0530

    Finished the routing for the pages.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,8 @@
 import MovieCard from "../components/MovieCard";
 import { useState } from "react";
 
-
-
 function Home() {
-    const [searchQuery, setSearchQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
 
   const movies = [
     { id: 1, title: "John Wick", release_date: "2020" },
@@ -12,7 +10,11 @@ function Home() {
     { id: 3, title: "The Matrix", release_date: "2020" },
   ];
 
-  const handleSearch = () => {};
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(searchQuery);
+    setSearchQuery("");
+  };
 
   return (
     <div className="home">
@@ -21,13 +23,20 @@ function Home() {
           type="text"
           placeholder="Search for movies...."
           className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
-        <button type="submit" className="search-button">Search</button>
+        <button type="submit" className="search-button">
+          Search
+        </button>
       </form>
       <div className="movies-grid">
-        {movies.map((movie) => (
-          <MovieCard movie={movie} key={movie.id} />
-        ))}
+        {movies.map(
+          (movie) =>
+            movie.title.toLowerCase().startsWith(searchQuery) && (
+              <MovieCard movie={movie} key={movie.id} />
+            )
+        )}
       </div>
     </div>
   );
